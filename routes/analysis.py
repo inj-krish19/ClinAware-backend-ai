8338c6c1457910de19260fdf170e6059bbc0d4d8
inj-krish19 <kglivee19@gmail.com>
2026-03-30 20:07:08 +0530

    Refactoring the response styles and Analysis

--- routes/analysis.py
+++ routes/analysis.py
@@ -3,8 +3,8 @@ import pandas as pd
 from flask import Blueprint, jsonify
 
 app = Blueprint("Analysis for Mediclaim related charts", __name__, url_prefix='/analysis')
-
 df = pd.read_csv('notebooks/medical_insurance.csv')
+
 
 @app.route("/")
 def index():
@@ -19,191 +19,165 @@ def index():
 age_avg_premium = df.groupby(by=['age'])['charges'].sum()
 age_avg_count = df.groupby(by=['age'])['charges'].count()
 
-data = {
-    '18-25': { 'min': 18, 'max': 25, 'charge': 0 },
-    '26-33': { 'min': 26, 'max': 33, 'charge': 0 },
-    '34-40': { 'min': 33, 'max': 40, 'charge': 0 },
-    '41-47': { 'min': 41, 'max': 47, 'charge': 0 },
-    '48-55': { 'min': 48, 'max': 55, 'charge': 0 },
-    '56-64': { 'min': 56, 'max': 64, 'charge': 0 }
-}
+data = [
+    { 'age': '18-25', 'min': 18, 'max': 25 },
+    { 'age': '26-33', 'min': 26, 'max': 33 },
+    { 'age': '34-40', 'min': 33, 'max': 40 },
+    { 'age': '41-47', 'min': 41, 'max': 47 },
+    { 'age': '48-55', 'min': 48, 'max': 55 },
+    { 'age': '56-64', 'min': 56, 'max': 64 }
+]
 
-
-for age_group in data.keys():
+for record in data:
 
     count = 0
     charge = 0
     
-    min_age = data[age_group]['min']
-    max_age = data[age_group]['max']
+    min_age = record['min']
+    max_age = record['max']
 
     for age in range(min_age, max_age+1):
         count += age_avg_count[age]
         charge += age_avg_premium[age]
     
-    data[age_group]['count'] = round(count, -2)
-    data[age_group]['cumulative'] = round(charge, -2)
-    data[age_group]['charge'] = round(charge // count, -2)
+    record['count'] = int(count)
+    record['total'] = float(round(charge))
 
-counts = [ d['count'] for d in list(data.values()) ]
-yearly_premium = [ d['charge'] for d in list(data.values()) ]
-monthly_premium = [ d['charge'] // 12 for d in list(data.values()) ]
+    record['yearly'] = float(round(charge // count, -2))
+    record['monthly'] = float(round(record['yearly'] // 12, -2))
 
-age_groups = list(data.keys())
 
-@app.route("/age-avg-premium-yearly")
-def age_average_premium_yearly():
-
+@app.route("/age-avg-premium")
+def age_average_premium():
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": age_groups,
-        "premium": list(yearly_premium)
-    })
-
-
-@app.route("/age-avg-premium-monthly")
-def age_average_premium_monthly():
-
-    return jsonify({
-        "code": 200,
-        "status": "OK",
-        "age": age_groups,
-        "premium": list(monthly_premium)
+        "data": data
     })
 
 
 
 # Smoker and Nonsmoker Analysis for Average Premium
-age_avg_premium = df.groupby(by=['smoker', 'age'])['charges'].sum()
-age_avg_count = df.groupby(by=['smoker', 'age'])['charges'].count()
+smoking_age_avg_premium = df.groupby(by=['smoker', 'age'])['charges'].sum()
+smoking_age_avg_count = df.groupby(by=['smoker', 'age'])['charges'].count()
 
-smoker_data_charge = age_avg_premium['yes']
-nonsmoker_data_charge = age_avg_premium['no']
+smoker_data_charge = smoking_age_avg_premium['yes']
+nonsmoker_data_charge = smoking_age_avg_premium['no']
 
-smoker_data_count = age_avg_count['yes']
-nonsmoker_data_count = age_avg_count['no']
-
-smoker_stats = {
-    '18-25': { 'min': 18, 'max': 25, 'charge': 0 },
-    '26-33': { 'min': 26, 'max': 33, 'charge': 0 },
-    '34-40': { 'min': 33, 'max': 40, 'charge': 0 },
-    '41-47': { 'min': 41, 'max': 47, 'charge': 0 },
-    '48-55': { 'min': 48, 'max': 55, 'charge': 0 },
-    '56-64': { 'min': 56, 'max': 64, 'charge': 0 }
-}
-
-nonsmoker_stats = {
-    '18-25': { 'min': 18, 'max': 25, 'charge': 0 },
-    '26-33': { 'min': 26, 'max': 33, 'charge': 0 },
-    '34-40': { 'min': 33, 'max': 40, 'charge': 0 },
-    '41-47': { 'min': 41, 'max': 47, 'charge': 0 },
-    '48-55': { 'min': 48, 'max': 55, 'charge': 0 },
-    '56-64': { 'min': 56, 'max': 64, 'charge': 0 }
-}
-
-for age_group in data.keys():
-
-    # 0 for Smoker, 1 for Nonsmoker
-    count = [0, 0]
-    charge = [0, 0]
-
-    # Same for both categories
-    min_age = smoker_stats[age_group]['min']
-    max_age = smoker_stats[age_group]['max']
-
-    for age in range(min_age, max_age+1):
-        count[0] += smoker_data_count[age]
-        charge[0] += smoker_data_charge[age]
-
-        count[1] += nonsmoker_data_count[age]
-        charge[1] += nonsmoker_data_charge[age]
-    
-    smoker_stats[age_group]['count'] = round(count[0], -2)
-    smoker_stats[age_group]['cumulative'] = round(charge[0], -2)
-    smoker_stats[age_group]['charge'] = round(charge[0] // count[0], -2)
-
-    nonsmoker_stats[age_group]['count'] = round(count[1], -2)
-    nonsmoker_stats[age_group]['cumulative'] = round(charge[1], -2)
-    nonsmoker_stats[age_group]['charge'] = round(charge[1] // count[1], -2)
+smoker_data_count = smoking_age_avg_count['yes']
+nonsmoker_data_count = smoking_age_avg_count['no']
 
 
 # SMOKER 
-smoker_counts = [ d['count'] for d in list(smoker_stats.values()) ]
-smoker_yearly = [ d['charge'] for d in list(smoker_stats.values()) ]
-smoker_monthly = [ d['charge'] // 12 for d in list(smoker_stats.values()) ]
-smoker_age_groups = list(smoker_stats.keys())
+smoker_stats = [
+    { 'age': '18-25', 'min': 18, 'max': 25 },
+    { 'age': '26-33', 'min': 26, 'max': 33 },
+    { 'age': '34-40', 'min': 33, 'max': 40 },
+    { 'age': '41-47', 'min': 41, 'max': 47 },
+    { 'age': '48-55', 'min': 48, 'max': 55 },
+    { 'age': '56-64', 'min': 56, 'max': 64 }
+]
+
+for record in smoker_stats:
+
+    count = 0
+    charge = 0
+
+    # Same for both categories
+    min_age = record['min']
+    max_age = record['max']
+
+    for age in range(min_age, max_age+1):
+        count += smoker_data_count[age]
+        charge += smoker_data_charge[age]
+    
+    record['count'] = int(count)
+    record['total'] = float(round(charge))
+
+    record['yearly'] =  float(round(charge // count, -2))
+    record['monthly'] = float(round(record['yearly'] // 12, -2))
 
 
-@app.route("/smoker-age-avg-premium-yearly")
-def smoker_age_average_premium_yearly():
+@app.route("/smoker-age-avg-premium")
+def smoker_age_average_premium():
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": smoker_age_groups,
-        "premium": list(smoker_yearly)
+        "data": smoker_stats
     })
 
 
-@app.route("/smoker-age-avg-premium-monthly")
-def smoker_age_average_premium_monthly():
+# NON SMOKER
+nonsmoker_stats = [
+    { 'age': '18-25', 'min': 18, 'max': 25 },
+    { 'age': '26-33', 'min': 26, 'max': 33 },
+    { 'age': '34-40', 'min': 33, 'max': 40 },
+    { 'age': '41-47', 'min': 41, 'max': 47 },
+    { 'age': '48-55', 'min': 48, 'max': 55 },
+    { 'age': '56-64', 'min': 56, 'max': 64 }
+]
+
+for record in nonsmoker_stats:
+
+    count = 0
+    charge = 0
+
+    # Same for both categories
+    min_age = record['min']
+    max_age = record['max']
+
+    for age in range(min_age, max_age+1):
+        count += nonsmoker_data_count[age]
+        charge += nonsmoker_data_charge[age]
+
+    record['count'] = int(count)
+    record['total'] = float(round(charge))
+
+    record['yearly'] =  float(round(charge // count, -2))
+    record['monthly'] = float(round(record['yearly'] // 12, -2))
+
+
+@app.route("/nonsmoker-age-avg-premium")
+def nonsmoker_age_average_premium():
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": smoker_age_groups,
-        "premium": list(smoker_monthly)
+        "data": data
     })
 
 
-# NON SMOKER 
-nonsmoker_counts = [ d['count'] for d in list(nonsmoker_stats.values()) ]
-nonsmoker_yearly = [ d['charge'] for d in list(nonsmoker_stats.values()) ]
-nonsmoker_monthly = [ d['charge'] // 12 for d in list(nonsmoker_stats.values()) ]
-nonsmoker_age_groups = list(smoker_stats.keys())
 
-
-@app.route("/nonsmoker-age-avg-premium-yearly")
-def nonsmoker_age_average_premium_yearly():
-    return jsonify({
-        "code": 200,
-        "status": "OK",
-        "age": nonsmoker_age_groups,
-        "premium": list(nonsmoker_yearly)
-    })
-
-
-@app.route("/nonsmoker-age-avg-premium-monthly")
-def nonsmoker_age_average_premium_monthly():
-    return jsonify({
-        "code": 200,
-        "status": "OK",
-        "age": nonsmoker_age_groups,
-        "premium": list(nonsmoker_monthly)
-    })
-
-
+# Region and Average Premium 
 region_avg_premium = df.groupby(by=['region'])['charges'].mean()
-region_age_groups = list(region_avg_premium.index)
-region_premium = [ round(premium, -2) for premium in region_avg_premium.values ]
+region_stats = {}
+
+for region in region_avg_premium.index:
+    region_stats[region] = round(region_avg_premium[region], -2) 
+
 
 @app.route("/region-avg-premium")
 def region_average_premium():
-
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": region_age_groups,
-        "premium": region_premium
+        "data": region_stats
     })
 
 
-region_average_premium = df.groupby(by=['smoker', 'region'])['charges'].mean()
 
-smoker_premium = region_average_premium['yes']
-nonsmoker_premium = region_average_premium['no']
+# Smoker Region Average Premium
+smoker_region_stats = []
+smoking_region_avg_premium = df.groupby(by=['smoker', 'region'])['charges'].mean()
 
-smoker_age_groups = list(smoker_premium.index)
-smoker_avg_premium = [ round(premium, -2) for premium in smoker_premium.values ]
+smoker_premium = smoking_region_avg_premium['yes']
+nonsmoker_premium = smoking_region_avg_premium['no']
+
+for region in smoker_premium.index:
+    smoker_region_stats.append({ 
+        'region': region, 
+        'yearly': round(smoker_premium[region], -2),
+        'yearly': round(smoker_premium[region] // 12, -2)
+    }) 
 
 @app.route("/smoker-region-avg-premium")
 def region_smoker_average_premium():
@@ -211,12 +185,19 @@ def region_smoker_average_premium():
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": smoker_age_groups,
-        "premium": smoker_avg_premium
+        "data": smoker_stats
     })
 
-nonsmoker_age_groups = list(nonsmoker_premium.index)
-nonsmoker_avg_premium = [ round(premium, -2) for premium in nonsmoker_premium.values ]
+
+
+# Nonsmoker Region Average Premium
+nonsmoker_region_stats = []
+for region in nonsmoker_premium.index:
+    nonsmoker_region_stats.append({
+        'region': region,
+        'yearly': round(nonsmoker_premium[region], -2),
+        'monthly': round(nonsmoker_premium[region] // 12, -2)
+    })
 
 @app.route("/nonsmoker-region-avg-premium")
 def region_nonsmoker_average_premium():
@@ -224,6 +205,5 @@ def region_nonsmoker_average_premium():
     return jsonify({
         "code": 200,
         "status": "OK",
-        "age": nonsmoker_age_groups,
-        "premium": nonsmoker_avg_premium
+        "data": nonsmoker_stats
     })
