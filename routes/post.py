c17f36590361240c633cb5917711f8f95ca196c0
inj-krish19 <kglivee19@gmail.com>
2026-05-06 10:27:07 +0530

    Sorting Post based on relevance

--- routes/post.py
+++ routes/post.py
@@ -14,6 +14,7 @@ def get_news():
         news['id'] = record.id
         data.append(news)
 
+    data.sort(key=lambda x: x['id'], reverse=True)
     return jsonify({
         "code": 200,
         "status": "OK",
